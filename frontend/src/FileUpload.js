4594f145889af470adf3c0fd24263767cf7bfc63
Bill Eger <egeriicw@gmail.com>
2021-11-26 21:40:06 -0500

    Add initial FileUpload component item

---
+++ frontend/src/FileUpload.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+const FileUpload = () => {
+    return <div>
+        File Uploading!
+    </div>;
+}
+
+export default uploadFile;
